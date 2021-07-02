5c0876d09671fcfd958016940d315ee39744fe7b
Mohamad <mrahmoun93@gmail.com>
2021-07-03 01:01:45 +0800

    joinecho.app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Field, Form } from 'formik';
+import { useState } from 'react';
 
 function App() {
+    const [showForm, setshowForm] = useState(true)
+    const [requestStatus, setRequestStatus] = useState(null)
+    const validate = (values, props) => {
+        const errors = {};
+
+        if (!values.email) {
+            errors.email = 'Email Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+        }
+        return errors;
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <div className="mb-2 orange-text">{requestStatus}</div>
+
+          <Formik
+        initialValues={{
+            email: '',
+        }}
+        validate={validate}
+        onSubmit={async (values) => {
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(values)
+            };
+             fetch('https://qa-api.joinecho.app/invitation_requests', requestOptions)
+                .then(response => response.json())
+                .then(data => setRequestStatus(data.status))
+                .then(() => setshowForm(false))
+                .catch((error) => {
+                    console.log('error: ' + error);
+                    setRequestStatus(error);
+                });;
+        }}
+    >
+        {(formik) => (
+        showForm && (<Form>
+            {formik.errors.email && <div className="mb-2 orange-text">{formik.errors.email}</div>}
+
+            <Field
+                id="email"
+                name="email"
+                placeholder="youremail@example.com"
+                type="email"
+                className="email-field"
+            />
+            <button type="submit" className="button mx-2">Submit</button>
+        </Form>)
+            )}
+    </Formik>
+      </div>
   );
 }
 
